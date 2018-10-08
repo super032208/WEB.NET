4092e0b7f74bdcda9658c517fb88a78889c540fe
super032208 <hdelcid@email.neit.edu>
2018-10-07 23:59:07 -0400

    first commit

---
+++ LABoNE/LABoNE.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LABoNE.aspx.cs" Inherits="LABoNE.LABoNE" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="result" runat="server" BackColor="#FFFF99" BorderColor="#3333CC" Font-Size="Large" Height="70px" Width="443px"></asp:TextBox>
+        </div>
+        <asp:Button ID="btn7" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn7_Click" Text="7" Width="109px" />
+        <asp:Button ID="btn8" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn8_Click" Text="8" Width="109px" />
+        <asp:Button ID="btn9" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn9_Click" Text="9" Width="109px" />
+        <asp:Button ID="btn_divide" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Font-Size="Large" Height="40px" OnClick="btn_divide_Click" Text="/" Width="109px" />
+        <br />
+        <asp:Button ID="btn4" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn4_Click" Text="4" Width="109px" />
+        <asp:Button ID="btn5" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn5_Click" Text="5" Width="109px" />
+        <asp:Button ID="btn6" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn6_Click" Text="6" Width="109px" />
+        <asp:Button ID="btn_mult" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn_mult_Click" Text="*" Width="109px" />
+        <br />
+        <asp:Button ID="btn1" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="Button9_Click" Text="1" Width="109px" />
+        <asp:Button ID="btn2" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn2_Click" Text="2" Width="109px" />
+        <asp:Button ID="btn3" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn3_Click" Text="3" Width="109px" />
+        <asp:Button ID="btn_sub" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn_sub_Click" Text="-" Width="109px" />
+        <br />
+        <asp:Button ID="btn_dot" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn_dot_Click" Text="." Width="109px" />
+        <asp:Button ID="btn_zero" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn_zero_Click" Text="0" Width="109px" />
+        <asp:Button ID="btn_add" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="btn_add_Click" Text="+" Width="109px" />
+        <asp:Button ID="btn_ce" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="41px" OnClick="btn_ce_Click" Text="CE" Width="109px" />
+        <br />
+        <asp:Button ID="btn_equal" runat="server" BackColor="#0066FF" BorderColor="#3333CC" Height="40px" OnClick="Button13_Click" Text="=" Width="443px" />
+    </form>
+</body>
+</html>
---
+++ LABoNE/LABoNE.aspx.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/*
+ Hector Delcid
+ Lab One 
+ Due: 10/09/18
+     
+ */
+
+
+namespace LABoNE
+{
+    public partial class LABoNE : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // all the calculator numbers 
+        #region numbers
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            result.Text += "1"; // #1 in the calculator
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            result.Text += "2"; // #2 in the calculator
+        }
+
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+            result.Text += "3"; // #3 in the calculator
+        }
+
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+            result.Text += "4"; // #4 in the calculator
+        }
+
+        protected void btn5_Click(object sender, EventArgs e)
+        {
+            result.Text += "5"; // #5 in the calculator
+        }
+
+        protected void btn6_Click(object sender, EventArgs e)
+        {
+            result.Text += "6"; // #6 in the calculator
+        }
+
+        protected void btn7_Click(object sender, EventArgs e)
+        {
+            result.Text += "7"; // #7 in the calculator
+        }
+
+        protected void btn8_Click(object sender, EventArgs e)
+        {
+            result.Text += "8"; // #8 in the calculator
+        }
+
+        protected void btn9_Click(object sender, EventArgs e)
+        {
+            result.Text += "9"; // #9 in the calculator
+        }
+        #endregion // all the  // all the 
+
+        // Zero and period
+        #region dot&zero
+        protected void btn_dot_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text; // period for decimal numbers
+        }
+
+        protected void btn_zero_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text; // #0 in the calculator 
+        }
+        #endregion
+
+        // calculator functions 
+        #region functions 
+        protected void btn_add_Click(object sender, EventArgs e)
+        {  // adding button 
+            Session["num1"] = result.Text;
+
+            Session["operator"] = "+";
+            result.Text = "";
+        }
+
+        protected void btn_sub_Click(object sender, EventArgs e)
+        { // subtract button 
+            Session["num1"] = result.Text;
+
+            Session["operator"] = "-";
+            result.Text = "";
+        }
+
+        protected void btn_mult_Click(object sender, EventArgs e)
+        { // multiply button
+            Session["num1"] = result.Text;
+
+            Session["operator"] = "*";
+            result.Text = "";
+        }
+
+        protected void btn_divide_Click(object sender, EventArgs e)
+        {   // divide button 
+            Session["num1"] = result.Text;
+
+            Session["operator"] = "/";
+            result.Text = "";
+        }
+        #endregion
+
+        // switch operator 
+        #region equal sign
+        protected void Button13_Click(object sender, EventArgs e)
+        { // equal button and all the function for add, sub, div and mult.
+
+            double num1, num2, num3 = 0;
+            string operand = Convert.ToString(Session["operator"]);
+            num1 = Convert.ToDouble(Session["num1"]);
+            num2 = Convert.ToDouble(result.Text);
+
+            switch (operand)
+            {
+                case "+":
+                    num3 = num1 + num2;// adding
+                break;
+
+                case "-":
+                    num3 = num1 - num2; // subtracting
+                    break;
+
+                case "*":
+                    num3 = num1 * num2; // multiply
+                    break;
+
+                case "/":
+                    num3 = num1 / num2;// dividing 
+                    break;
+
+                default:
+                    break;
+
+
+            }
+
+            result.Text = Convert.ToString(num3); // converting string to double 
+
+
+        }
+
+        
+
+        protected void btn_ce_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";// return to zero
+        }
+
+        #endregion
+    }
+}
